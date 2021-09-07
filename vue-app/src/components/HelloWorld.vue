54e69b495c07b91bc508031ea65cf2f5f3fd829a
tanaka <k-tanaka@kodensya.co.jp>
2021-09-07 16:00:47 +0900

    first build
    first build(test)

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <input v-model="msg" />
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">たなかで～す</a>.
+    </p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
